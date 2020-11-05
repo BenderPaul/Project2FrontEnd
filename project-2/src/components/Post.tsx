5d7a64c49e3aef3aab11895f7a50880ec904b95c
cliftonwferguson <Clifton.ferguson7@gmail.com>
2020-11-05 15:04:11 -0600

    building post page

--- project-2/src/components/Post.tsx
+++ project-2/src/components/Post.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux'
 //placeholder
 export const Props: React.FC = () => {
+    const posts = useSelector(state => state.initialState)
+    
+    const renderedPosts = posts.map(post => (
+        <article className="post-excerpt" key={post.postId}>
+            <h3>{post.title}</h3>
+        <p>{post.body.substring(0, 100)}</p>
+             <h3>{post.author}</h3>
+        </article>
+    ))
+    
+    
     return (
         <div>
-            
+
         </div>
     );
 }
