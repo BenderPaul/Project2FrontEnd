623e7b10e404d85b7e32891db1ad1d7e2c781bc7
cliftonwferguson <Clifton.ferguson7@gmail.com>
2020-11-05 15:04:11 -0600

    building post page
    rebase continue

--- project-2/src/components/Post.tsx
+++ project-2/src/components/Post.tsx
@@ -5,11 +5,13 @@ import { store } from '../Store';
 export const Post: React.FC = () => {
     const post = store.getState().PostState.posts;
     const renderedPosts = () => (
+
         <article className="post-excerpt" key={post.postId}>
             <h3>{post.title}</h3>
         <p>{post.body.substring(0, 100)}</p>
              <h3>{post.author}</h3>
         </article>
+
 
     )
     return (
