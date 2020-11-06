8dbb237d47dadaa7d13794b1eb9328b96b3a7aa4
cliftonwferguson <Clifton.ferguson7@gmail.com>
2020-11-05 19:40:33 -0600

    rebasing master

--- project-2/src/components/Post.tsx
+++ project-2/src/components/Post.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { useSelector } from 'react-redux'
+import { postReducer } from '../reducers/postReducer'
+import { IState } from '../interfaces'
+
+
 //placeholder
 export const Props: React.FC = () => {
-    const posts = useSelector(state => state.initialState)
+    const posts = useSelector((state: IState) => state.PostState)
     
-    const renderedPosts = posts.map(post => (
-        <article className="post-excerpt" key={post.postId}>
-            <h3>{post.title}</h3>
-        <p>{post.body.substring(0, 100)}</p>
-             <h3>{post.author}</h3>
+    const renderedPosts = ()  =>  (
+        <article className="post-excerpt" key={posts.postId}>
+            <h3>{posts.title}</h3>
+        <p>{posts.body.substring(0, 100)}</p>
+             <h3>{posts.author}</h3>
         </article>
-    ))
+    )
     
     
     return (
         <div>
-
+           <h2>Posts</h2>
+           {renderedPosts}
         </div>
     );
 }
