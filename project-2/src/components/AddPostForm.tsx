4e435bbd87dc7f7e9cd9cdcd550d179a40de5e61
cliftonwferguson <Clifton.ferguson7@gmail.com>
2020-11-07 19:59:04 -0600

    submit post structure

--- project-2/src/components/AddPostForm.tsx
+++ project-2/src/components/AddPostForm.tsx
@@ -1,8 +1,37 @@
 import React, { useState } from 'react'
-const [title, setTitle] = useState('')
-const [content, setContent] = useState('')
 
-const onTitleChanged = e => 
+export const AddPostForm = () => {
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
 
+  const onTitleChanged = (e) => setTitle(e.target.value)
+  const onContentChanged = (e) => setContent(e.target.value)
 
+  return (
+      <section>
+          <h2>Add a New Post</h2>
+          <form>
+              <label htmlFor="postTitle">Post Title</label>
+              <input
+                type="text"
+                id="postTitle"
+                name="postTitle"
+                value={title}
+                onChange={onTitleChanged}
+               />
+              <label htmlFor="postBody">Body:</label>
+              <textarea
+                id="postContent"
+                name="postContent"
+                value={content}
+                onChange={onContentChanged}
+              />
+             <button type="button">Save Post</button>     
+          </form>
+      </section>
+  )
+
+  
+
+}
 
