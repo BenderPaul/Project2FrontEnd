263e4c22e5b9c9f45a191e809b22993911d3e3c7
ar-pearse <andrew.roy.pearse@gmail.com>
2020-11-10 23:08:52 -0600

    Filled out a bunch of components

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,43 +1,33 @@
 import React from 'react';
+import { Form, Button, Input } from 'reactstrap';
 import { IPost, IUser } from '../interfaces';
 import { Post } from './Post';
 
 export const Home: React.FC = () => {
 
-    // const [posts, setPosts] = React.useState(null);
-
-    // window.onload = async () => {
-    //     const response = await Axios.get('http://34.211.139.29:8081/StickyDB/user/allusers');
-    //     const json = await JSON.stringify(response.data);
-
-    //     const obj = Object.entries(await response.data);
-
-    //     for(const user in obj) {
-    //         console.log(obj[user]);
-    //     }
-
-    //     setPosts(json);
-    // }
-
-    const post:IPost = {
-        postId: 0,
-        author: "",
-        title: "",
-        body: "",
-        likes: 0
+    const user:IUser = {
+        id: 0,
+        username: "",
+        password: "",
+        email: "",
+        firstName: "",
+        lastName: "",
+        bio: "",
+        occupation: "",
+        dob: "",
+        phoneNumber: "",
+        address: ""
     };
 
-    const posts = <Post {...post}/>;
+
 
     return (
         <div>
-            <h1>Home page</h1>
-            <ul>
-                Included:
-                <li>feed</li>
-                <li>ability to search for other users</li>
-            </ul>
-            {posts}
+            <Form inline>
+            <Input type="text" placeholder="Search users" className="mr-sm-2"/>
+            <Button variant="outline-primary">Search</Button>
+            </Form>
+            <Post {...user}/>
         </div>
     )
 }
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,15 +1,28 @@
 import Axios from 'axios';
 import React, { useState } from 'react';
-import { IUser } from '../interfaces';
+import { emptyUser } from '../interfaces';
 import { loadState } from '../Store';
 
 export const Profile: React.FC = () => {
 
-    const [userProfile, setUserProfile] = useState(null);
+    console.log("here")
 
-    const refresh = (user:IUser = loadState()) => {
-        console.log(user.username);
+    const [userProfile, setUserProfile] = useState(emptyUser);
+
+    console.log(loadState());
+
+    const getUser = async (username = loadState()) => {
+        const response = await Axios.post(`http://34.211.139.29:8081/StickyDB/user/findbyusername`, {
+            params: {
+                username: username
+            }
+        });
+        setUserProfile(response.data[0]);
     }
+
+    getUser();
+
+
 
     return (
         <div>
@@ -20,7 +33,25 @@ export const Profile: React.FC = () => {
                 <li>View current profile information</li>
             </ul>
 
-            
+            <h1>
+                Name: {userProfile.firstName} {userProfile.lastName}
+            </h1>
+            <h3>
+                Email: {userProfile.email}
+            </h3>
+            <h4>
+                Username: {userProfile.username}
+                Phone Number: {userProfile.phoneNumber}
+                Address: {userProfile.address}
+            </h4>
+            <br/><br/>
+            <h3>
+                Birthday: {userProfile.dob}
+                Occupation: {userProfile.occupation}
+            </h3>
+            <h3>
+                Bio: {userProfile.bio}
+            </h3>
         </div>
     );
 }
